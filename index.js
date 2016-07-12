8c7a4e0f090ebc0ef6c152a4ff77363da9a832c0
Sean Nicholas Dieterle <n.dieterle@netvise.de>
2016-07-12 11:06:25 +0200

    Refactored

--- index.js
+++ index.js
@@ -8,101 +8,53 @@ const ffmpegPath = config.ffmpegPath;
 
 const fs = require('fs');
 const TelegramBot = require('node-telegram-bot-api');
-const http = require('https');
 const _ = require('lodash');
-const chatSettings = require('./chatSettings');
-
-const ffmpeg = (args = []) => {
-  return new Promise((res, rej) => {
-    args.unshift('-loglevel error');
-    const argString = args.join(' ');
-    require('child_process').exec(ffmpegPath + ' ' + argString, (err, stdout, stderr) => {
-      if (err) {
-        return rej(err);
-      }
-
-      if (stderr) {
-        return rej(stderr);
-      }
-
-      res(stdout);
-    });
-  });
-};
-
-const speechToText = (file, language) => {
-  return new Promise((resolve) => {
-    if (!witToken[language]) {
-      language = 'EN';
-    }
-
-    const options = {
-      "method": "POST",
-      "hostname": "api.wit.ai",
-      "port": null,
-      "path": "/speech?v=20160706",
-      "headers": {
-        "authorization": "Bearer " + witToken[language],
-        "content-type": "audio/mpeg3",
-        "cache-control": "no-cache"
-      }
-    };
-
-    const req = http.request(options, function (res) {
-      const chunks = [];
-
-      res.on("data", function (chunk) {
-        chunks.push(chunk);
-      });
-
-      res.on("end", function () {
-        const body = Buffer.concat(chunks);
-        resolve(JSON.parse(body.toString()));
-      });
-    });
-
-    fs.createReadStream(file).pipe(req);
-  });
-};
+const chatSettings = require('./lib/chatSettings');
+const ffmpeg = require('./lib/ffmpeg');
+const speechToText = require('./lib/speechToText');
+const commands = require('./lib/commands');
 
 // Setup polling way
 const bot = new TelegramBot(telegramToken, { polling: true });
+commands.commandEvent(bot, config.botName);
 
-var setLanguage = function(msg, lngText){
+const setLanguage = function(msg, lngText) {
   const replyMarkup = {reply_markup: {hide_keyboard: true}};
   const language = _.upperCase(lngText);
+
   if (!witToken[language]) {
     bot.sendMessage(msg.chat.id, 'Parrot does not know this language *flies away*', replyMarkup);
     return;
   }
+
   chatSettings.set(msg.chat.id, { 'language': language }).then(() => {
-    if(language == 'DE'){
+    if (language == 'DE'){
       bot.sendMessage(msg.chat.id, 'Parrot versteht jetzt deutsch!', replyMarkup);
-    }else if (language == 'EN') {
+    } else if (language == 'EN') {
       bot.sendMessage(msg.chat.id, 'Parrot is now listening to english!', replyMarkup);
     }
-
   }).catch((err) => {
     bot.sendMessage(msg.chat.id, 'Ahhh error, errrorrrrrrr!', replyMarkup);
-    console.log('Language change err', err)
+    console.log('Language change err', err);
   });
 }
 
 chatSettings.load().then(() => {
   bot.on('message', function (msg) {
+    console.log(msg);
     if (!msg.voice) {
       return;
     }
 
     bot.downloadFile(msg.voice.file_id, __dirname + '/tmp').then(filePath => {
       const mp3Path = filePath + '.mp3'
-      return ffmpeg(['-i ' + filePath, '-acodec libmp3lame', mp3Path]).then((stdout) => {
+      return ffmpeg(ffmpegPath, ['-i ' + filePath, '-acodec libmp3lame', mp3Path]).then((stdout) => {
         //TODO: Add callback & promisify
         fs.unlink(filePath);
         return mp3Path;
       });
     }).then(file => {
-      return speechToText(file, chatSettings.get(msg.chat.id).language).then(response => {
+      return speechToText(witToken, file, chatSettings.get(msg.chat.id).language).then(response => {
         fs.unlink(file);
         return response;
       });
@@ -131,19 +83,28 @@ chatSettings.load().then(() => {
     });
   });
 
-  bot.onText(/\/setlanguage (.+)|\/setlanguage/, function (msg, match) {
+  bot.onCommand("/setlanguage", function (msg, match) {
+    console.log(match);
     const language = _.upperCase(match[1]);
 
     if (witToken[language]){
       setLanguage(msg, language);
-    }else{
-      const keyboardOpts = {reply_markup:{keyboard: [['DE'], ['EN']], one_time_keyboard: true, selective: true}}
+    } else {
+      const keyboardOpts = {
+        reply_markup: {
+          keyboard: [['DE'], ['EN']],
+          one_time_keyboard: true,
+          selective: true
+        }
+      };
+
       bot.sendMessage(msg.chat.id, 'Which language?', keyboardOpts).then(() => {
-      chatSettings.set(msg.chat.id, { 'waitForLanguageResponse': true });
-    });
+        chatSettings.set(msg.chat.id, { 'waitForLanguageResponse': true });
+      });
   }
+
   bot.onText(/(DE)|(EN)/, function(msg, match){
-      if(!chatSettings.get(msg.chat.id).waitForLanguageResponse){
+      if (!chatSettings.get(msg.chat.id).waitForLanguageResponse) {
         return;
       }
       setLanguage(msg, match[0]);
